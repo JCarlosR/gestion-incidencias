1e7c6b8ae51c0a3e3cc787fce98a2dee3dd53c26
Juan Carlos <juancagb.17@gmail.com>
2017-01-26 09:05:14 -0500

    Pie chart data by state

--- app/Http/Controllers/Api/IncidentController.php
+++ app/Http/Controllers/Api/IncidentController.php
@@ -5,10 +5,35 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Incident;
+
 class IncidentController extends Controller
 {
-    public function state()
+    public function stateCount()
     {
-    	return Incident::all();
+    	$incidents = Incident::all();
+
+    	$pending = 0;
+    	$solved = 0;
+    	$assigned = 0;
+
+		foreach ($incidents as $incident) {
+			switch ($incident->state) {
+				case 'Resuelto':
+					++$solved;
+					break;
+				case 'Asignado':
+					++$assigned;
+					break;
+				default:
+					++$pending;
+					break;
+			}
+		}
+
+		$data['pending'] = $pending;
+		$data['solved'] = $solved;
+		$data['assigned'] = $assigned;
+    	return $data;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::get('/projects', 'Api\ProjectController@all');
 Route::get('/project/categories', 'Api\CategoryController@byProject');
 
 // Android API - Reports
-Route::get('/incidents/state', 'Api\IncidentController@state');
+Route::get('/incidents/state', 'Api\IncidentController@stateCount');
 
 // Web API
 Route::get('/proyecto/{id}/niveles', 'Admin\LevelController@byProject');
