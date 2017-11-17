496ea9800d44fcacedb70f9b888999fe4080b263
Juan Carlos <juancagb.17@gmail.com>
2017-11-17 09:05:20 -0500

    Update CSS link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.css">
+    <link rel="stylesheet" href="//bootswatch.com/3/flatly/bootstrap.css">
     @yield('styles')
 
     <!-- Scripts -->
