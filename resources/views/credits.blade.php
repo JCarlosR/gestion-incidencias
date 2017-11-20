4a257bcaab7cdd09a9aa17b93ec663e909710f37
Juan Carlos <juancagb.17@gmail.com>
2017-11-20 13:18:57 -0500

    Remove useless css & change texts

--- resources/views/credits.blade.php
+++ resources/views/credits.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.app')
 
-@section('styles')
-<style>
-	.img-responsive {
-    	margin: 0 auto;
-	}
-</style>
-@endsection
-
 @section('content')
 <div class="panel panel-primary">
     <div class="panel-heading">Sistema de gestión de incidencias</div>
     <div class="panel-body text-center">
-        <p>Aqui va tu nombre</p>
+        <p>Página de créditos. Pendiente por editar.</p>
     </div>
 </div>
 @endsection
